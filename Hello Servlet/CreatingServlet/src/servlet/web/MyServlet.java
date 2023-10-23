77f5b8ad3595b3dcaa6f225930ed8387263e84b0
Km Shiwangi <73150948+Shiv55293@users.noreply.github.com>
2023-10-23 12:26:33 +0530

    Creating Servlet
    Creating Servlet using HttpServlet class including web.xml file

---
+++ Hello Servlet/CreatingServlet/src/servlet/web/MyServlet.java
@@ -0,0 +1,23 @@
+package servlet.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import org.apache.catalina.connector.Response;
+
+public class MyServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = resp.getWriter();
+		out.print("I am in doGet() method");
+		//System.out.println("This is doGet() method! ");
+	}
+
+}
